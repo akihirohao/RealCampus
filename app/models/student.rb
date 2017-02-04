2b19d8fb9bd9d94c85273be64b822f8658991b76
tech-camp <t.a12752t@gmail.com>
2017-02-04 14:35:56 +0900

    今からモデル内カラムを作業する（モデル間は出来てる）

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :reservations
+  has_many :users
+
 end
