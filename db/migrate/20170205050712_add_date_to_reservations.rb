e9324f558b20ab4f6004ad3b514a90ef9ca6fcd7
tech-camp <t.a12752t@gmail.com>
2017-02-19 20:20:56 +0900

    11limit部分を消してlocalのテーブル編集後

--- db/migrate/20170205050712_add_date_to_reservations.rb
+++ db/migrate/20170205050712_add_date_to_reservations.rb
@@ -1,5 +1,5 @@
 class AddDateToReservations < ActiveRecord::Migration
   def change
-    add_column :reservations, :date, :integer, :limit => 11
+    add_column :reservations, :date, :integer
   end
 end
